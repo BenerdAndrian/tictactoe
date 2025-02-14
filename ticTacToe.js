5ab09bacc8e1833621778952fb5e0d007c65ed8d
BenerdAndrian <ben527466@gmail.com>
2025-02-14 08:16:40 +0000

    in HTML,adding p.paranote and button.close
     in CSS, styling font-size of h1,increase width of div.notification to 650px, adding padding:1rem,styling close button
    
    in JS,delete toCheck(),getColumnCount(),getRowCount(),diagnalCount() as it not necessary anymore. using DOM manipulate to add event to close button,check if the result has a winner before checking ties using winnerDeclared variable, add resetBoard() function.

--- ticTacToe.js
+++ ticTacToe.js
@@ -4,18 +4,17 @@ const Gameboard=(function(){
     [" "," "," "],
     [" "," "," "]
   ]
+  let count1=0;
+  let count2=0;
   const toMark=(mark,row,column)=>{
         board[row][column]=mark;
   }
   const toMarkDOM=()=>{
     const squareDiv=document.querySelectorAll(".squareDiv");
-    let count1=0;
-    let count2=0;
     squareDiv.forEach(div=>{
       div.addEventListener("click",function(){
         let row=div.dataset.row;
         let col=div.dataset.col;
-        
         if(div.textContent===""){
           console.log("bi trong");
           const p=document.createElement("p");
@@ -45,15 +44,7 @@ const Gameboard=(function(){
       })
     })
   }
-  const toCheck=()=>{
-    for(let i=0;i<board.length;i++){
-        for(let j=0;j<board[i].length;j++){
-            if(board[i][j]===" ") console.log("true");
-            else if(board[i][j]!==" ") console.log("false");
-        }
-        
-    }
-  }
+ 
   const toDraw=()=>{
     for(let i=0;i<board.length;i++){
         const array=[];
@@ -63,39 +54,13 @@ const Gameboard=(function(){
         console.log(array);
     }
   }
-  const getRowCount=()=>{
-    let flagX=0;
-    let flagO=0;
-    for(let i=0;i<board.length;i++){
-          flagX=0;flagO=0;
-        for(let j=0;j<board[i].length;j++){
-            if(board[i][j]==="X") flagX++;
-            else if(board[i][j]==="O") flagO++
-            if(flagX===3){
-                return true;
-            }else if(flagO===3){
-                return true;
-            }
-        }
-    }
-  }
-  const getColumnCount=()=>{
-    let i=0;
-    for(let j=0;j<3;j++){
-        if(board[0][j]===board[1][j]===board[2][j]==="X") return true;
-        else if(board[0][j]===board[1][j]===board[2][j]==="O") return true;
-    }
-  }
-  const getDiagonalCount=()=>{
-    if(board[0][0]===board[1][1]===board[2][2]==="X") console.log("X wins");
-    else if(board[0][0]===board[1][1]===board[2][2]==="O") return true;
-    else if(board[0][2]===board[1][1]===board[2][0]==="X") return true;
-    else if(board[0][2]===board[1][1]===board[2][0]==="O") return true;
-  }
+  
   const resetBoard=()=>{
     const squareDiv=document.querySelectorAll(".squareDiv")
+    const paragraph=document.querySelector(".paraNote")
     squareDiv.forEach(div=>{
-      div.innerHTML=" ";
+      div.innerHTML="";
+      paragraph.textContent="";
     })
     for(let i=0;i<board.length;i++){
       for(let j=0;j<board[i].length;j++){
@@ -103,40 +68,68 @@ const Gameboard=(function(){
           
       }
     }
+    count1=0;
+    count2=0;
   }
-  const giveResult=()=>{
-     // Check rows
-     let result='';
-     const notification=document.querySelector(".notification");
-     const p=document.createElement("p");
-
-    for(let i=0;i<3;i++){
-      //check rows
-      if(board[i][0]===board[i][1]&&board[i][1]===board[i][2]&&board[i][0]!=" "){
-        board[i][0]==="X"? p.textContent="Player1 wins the game":p.textContent="Player2 wins the game";
-        notification.appendChild(p);
-        notification.style.display="block";
+  const closeUp=()=>{
+  const closebtn=document.querySelector(".close")
+  const notification=document.querySelector(".notification")
+  closebtn.addEventListener("click",()=>{
+    notification.style.display="none";
+    resetBoard();
+  })
+  }
+  const giveResult = () => {
+    const notification = document.querySelector(".notification");
+    const p = document.querySelector(".paraNote");
+    const close = document.querySelector(".close");
+    const squareDiv = document.querySelectorAll(".squareDiv");
+    const paraNote = document.querySelector(".paraNote");
+    
+    let cell = 0;
+    let winnerDeclared = false; // To prevent further checking after a winner is found
+  
+    // Function to handle game result notification
+    const declareWinner = (player) => {
+      p.textContent = `${player === "X" ? "Player1" : "Player2"} wins the game`;
+      notification.style.display = "block";
+      closeUp();
+      winnerDeclared = true;  // Stop further checks once a winner is found
+    };
+  
+    // Check rows and columns for a winner
+    for (let i = 0; i < 3; i++) {
+      // Check row
+      if (board[i][0] === board[i][1] && board[i][1] === board[i][2] && board[i][0] !== " ") {
+        declareWinner(board[i][0]);
       }
-      //check columns
-      else if(board[0][i]===board[1][i]&&board[1][i]===board[2][i]&&board[0][i]!=" "){
-        board[0][i]==="X"? p.textContent="Player1 wins the game":p.textContent="Player2 wins the game";
-        notification.appendChild(p);
-        notification.style.display="block";
+      // Check column
+      else if (board[0][i] === board[1][i] && board[1][i] === board[2][i] && board[0][i] !== " ") {
+        declareWinner(board[0][i]);
       }
-    }
-    if(board[0][0]===board[1][1] && board[1][1]===board[2][2]&& board[1][1]!=" "){
-      board[0][0]==="X"? p.textContent="Player1 wins the game":p.textContent="Player2 wins the game";
-      notification.appendChild(p);
-      notification.style.display="block";
-    }
-    else if(board[0][2]===board[1][1] && board[1][1]===board[2][0]&& board[1][1]!=" "){
-      board[0][2]==="X"? p.textContent="Player1 wins the game":p.textContent="Player2 wins the game";
-      notification.appendChild(p);
-      notification.style.display="block";
     }
   
-}
+    // Check diagonals for a winner
+    if (!winnerDeclared) {
+      if (board[0][0] === board[1][1] && board[1][1] === board[2][2] && board[0][0] !== " ") {
+        declareWinner(board[0][0]);
+      } else if (board[0][2] === board[1][1] && board[1][1] === board[2][0] && board[0][2] !== " ") {
+        declareWinner(board[0][2]);
+      }
+    }
   
+    // Check if the game is tied (only if no winner declared)
+    if (!winnerDeclared) {
+      squareDiv.forEach((div) => {
+        if (div.textContent !== "") cell++;
+        if (cell === 9) {
+          paraNote.textContent = "The Game is tied.";
+          notification.style.display = "block";
+          closeUp();
+        }
+      });
+    }
+  };
 
 
   const displayUIGameBoard=()=>{
@@ -154,24 +147,7 @@ const Gameboard=(function(){
     
     }
   }
- const takeTurn=()=>{
-    let count1=0;
-    let count2=0;
-    let currentPlayer=player1;
-    for(let i=0;i<9;i++){
-      if(count1>count2){
-        toMarkDOM(player2.mark);
-        console.log(player2.mark)
-        count2++;
-      }
-      else if(count1===count2){
-        currentPlayer=player1;
-        toMarkDOM(player1.mark);
-        console.log(player1.mark)
-        count1++;
-      }
-    }
- }
+ 
   const player1={
     mark:"X",
   }
@@ -179,13 +155,11 @@ const Gameboard=(function(){
     mark:"O",
   }
   return{
-    toCheck,
     toMark,
     toDraw,
     giveResult,
     displayUIGameBoard,
     toMarkDOM,
-    takeTurn,
   }
 })();
 
